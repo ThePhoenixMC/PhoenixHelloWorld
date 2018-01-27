060034103cf512a5b70b8b32bd6fafa13dbace9b
lss233 <m18760286457@163.com>
2018-01-28 00:29:12 +0800

    Update due to API changes.

--- src/main/java/com/lss233/demo/Demo.java
+++ src/main/java/com/lss233/demo/Demo.java
@@ -1,7 +1,6 @@
 package com.lss233.demo;
 
 import com.lss233.phoenix.Phoenix;
-import com.lss233.phoenix.config.ConfigurationManager;
 import com.lss233.phoenix.config.json.JsonConfiguration;
 import com.lss233.phoenix.logging.Logger;
 import com.lss233.phoenix.module.Module;
@@ -17,16 +16,17 @@ public class Demo extends Module{
     private Logger logger = Phoenix.getLogger("DemoModule");
     @Override
     public void onInitial() {
-
+        logger.info("Initializing ...");
     }
 
     @Override
     public void onEnable() {
-        logger.info("Demo has enabled.");
+        logger.info("Enabling ...");
         Phoenix.getCommandManager().registerCommand(this,new DemoCommandExecutor());
 
         Phoenix.getEventManager().registerListener(this, new DemoEventListener());
         try {
+            logger.debug(Phoenix.getConfigurationManager().getConfigurationDirectory(this).getAbsolutePath());
             JsonConfiguration configuration = Phoenix.getConfigurationManager().getConfig(this);
             configuration.set("blah", "2");
             configuration.save();
